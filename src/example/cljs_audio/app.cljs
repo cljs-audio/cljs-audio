b9c3f9d48e01d73ec6fe94c6de565322dde85c39
Nikita Dudnik <nikdudnik@gmail.com>
2021-09-23 12:58:22 +0200

    Update example

--- src/example/cljs_audio/app.cljs
+++ src/example/cljs_audio/app.cljs
@@ -1,71 +1,86 @@
 (ns cljs-audio.app
   (:require
     [cljs-audio.webaudio :as wa]
-    [cljs-audio.envelopes :refer [adsr! adsr at-time!]]
+    [cljs-audio.envelopes :refer [adsr! adsr at-time! at-time]]
     [cljs-audio.scheduling :as s]
     [cljs-audio.time :as t]
     [cljs-audio.modules :as m]
-    [cljs.core.async :refer [go]]
+    [cljs.core.async :refer [go <!]]
     [cljs.core.async.interop :refer-macros [<p!]]
     ["standardized-audio-context" :refer (AudioContext)]))
 
-(defn graph [{:keys [events ir]}]
-  (let [{:keys [frequency gain]} (s/schedule-parameters {:frequency at-time! :gain (partial adsr! 0.1 0.15 0.6 0.5 0.2)}
-                                                        events)]
-    [{:voice (m/simple-voice {:frequency frequency
-                              :gain      gain
-                              :type      "sawtooth"})
-      :voice2 (m/simple-voice {:frequency frequency
-                               :detune    -4
+(defn graph [{:keys [ir kick time bpm freq]}]
+  (let [{:keys [frequency gain]} (s/schedule-parameters {:frequency at-time :gain (partial adsr 0.2 0.15 0.6 0.5 0.2)}
+                                                        [{:frequency freq :gain 0.3 ::s/time (+ time 0)}
+                                                         {:frequency (* 2 freq) :gain 0.3 ::s/time (+ time (t/seconds 120 "1/4"))}
+                                                         {:frequency (* 4 freq) :gain 0.3 ::s/time (+ time (t/seconds 120 "1/4")
+                                                                                                    (t/seconds 120 "1/4"))}])]
+    [{:voice  (m/simple-voice {:frequency frequency
                                :gain      gain
-                               :type      "sine"})
-      :reverb [:convolver {:buffer ir}]
-      :fx    (m/multi-tap-delay {:times (mapv m/at-start [(t/seconds 120 "1/4")
-                                                          (t/seconds 120 "1/2")
-                                                          (t/seconds 120 "1")]) :gains (mapv m/at-start [0.5 0.25 0.125 0.05])})
-      :vca    [:gain {:gain 0.3}]
-      :comp  [:dynamics-compressor {:threshold -50 :knee 0 :ratio 20 :attack 0.005 :release 0.050}]
+                               :type      "sawtooth"})
+      :sampled-kick (m/poly m/one-shot-sample
+                            [{:buffer kick :start-time time :gain gain :rate 0.7}
+                             {:buffer kick :start-time (+ time (t/seconds bpm "1/4")) :gain gain :rate 0.7}
+                             {:buffer kick :start-time (+ time (t/seconds bpm "2/4")) :gain gain :rate 0.7}
+                             {:buffer kick :start-time (+ time (t/seconds bpm "3/4")) :gain gain :rate 0.7}])
+      ;:voice2 (m/simple-voice {:frequency frequency
+      ;                         :detune    -4
+      ;                         :gain      gain
+      ;                         :type      "sine"})
+      ;:reverb [:convolver {:buffer ir}]
+      :fx     (m/multi-tap-delay {:times (mapv m/at-start [(t/seconds bpm "1/2")
+                                                           (t/seconds bpm "1")
+                                                           (t/seconds bpm "2")]) :gains (mapv m/at-start [0.5 0.25 0.125 0.05])})
+      ;:vca    [:gain {:gain 0.3}]
+      ;:comp   [:dynamics-compressor {:threshold -50 :knee 0 :ratio 20 :attack 0.005 :release 0.050}]
       }
-     #{[:voice2 :vca]
-       [:vca :reverb]
-       [:reverb :comp]
+     #{
+       ;[:voice2 :vca]
+       ;[:vca :comp]
+       ;[:reverb :>]
+       ;[:voice :fx]
        [:voice :fx]
-       [:fx :comp]
-       [:comp :>]
+       [:fx :>]
+       [:sampled-kick :>]
+       ;[:comp :reverb]
+       ;[:comp :>]
+       ;[:sampled-kick :comp]
+       ;[:fx :comp]
        }]))
 
-(def audio (atom nil))
+(defonce audio (atom nil))
 
 (def events ["touchstart" "touchend" "mousedown" "keydown"])
 
-(defn update-audio [{:keys [freq time stream ir]}]
-  (swap! audio wa/update-audio (graph {:ir ir :events [{:frequency freq :gain 0.1 ::s/time (+ time 0)}
-                                                       {:frequency (* 2 freq) :gain 0.1 ::s/time (+ time (t/seconds 120 "1/4"))}
-                                                       {:frequency (* 4 freq) :gain 0.1 ::s/time (+ time (t/seconds 120 "1/4") (t/seconds 120 "1/4"))}]})))
+(defn fetch-sample [ctx path] (go (->> (js/fetch path)
+                                       (<p!)
+                                       (.arrayBuffer)
+                                       (<p!)
+                                       (.decodeAudioData ctx)
+                                       (<p!))))
 
 (defn resume-audio-context []
   (let [ctx (new AudioContext)]
     (.then (.resume ctx)
            (fn []
              (go
-               (let [ir-mp3 (<p! (js/fetch "resources/ir.mp3"))
-                     ir-audio-buffer (<p! (.arrayBuffer ir-mp3))
-                     ir-audio-data (<p! (.decodeAudioData ctx ir-audio-buffer))]
+               (let [ir-audio-data (<! (fetch-sample ctx "resources/ir.wav"))
+                     kick-audio-data (<! (fetch-sample ctx "resources/kick.wav"))
+                     bpm 120]
                  (try
                    (doseq [ev events] (.removeEventListener js/document.body ev resume-audio-context))
                    (js/document.body.addEventListener "mousedown"
-                                                      (fn [e] (update-audio {:freq (/ (.-offsetY e) 10)
-                                                                             :time (.-currentTime ctx)
-                                                                             :ir   ir-audio-data})))
+                                                      (fn [e]
+                                                        (.preventDefault e)
+                                                        (swap! audio wa/update-audio (graph {:kick kick-audio-data :freq (/ (.-offsetY e) 10) :ir ir-audio-data :time (.-currentTime ctx) :bpm bpm}))))
                    (js/document.body.addEventListener "touchstart"
                                                       (fn [e]
+                                                        (.preventDefault e)
                                                         (let [touch (.item (.-changedTouches e) 0)]
                                                           (when (not (nil? touch))
-                                                            (update-audio {:freq (/ (.-offsetY e) 5)
-                                                                           :time (.-currentTime ctx)
-                                                                           :ir   ir-audio-data}))
-                                                          ))
-                                                      (reset! audio (wa/make-audio ctx nil)))
+                                                            (swap! audio wa/update-audio (graph {:kick kick-audio-data :freq (/ (.-pageY touch) 5) :ir ir-audio-data :time (.-currentTime ctx) :bpm bpm}))))
+                                                        ))
+                   (reset! audio (wa/make-audio ctx nil))
                    (catch js/Error err (js/console.log (ex-cause err))))))))))
 
 (defn main []
